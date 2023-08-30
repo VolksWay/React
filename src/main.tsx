f9160ec663622521024e3a25525d661e9d304415
Alan Munoz Martins <alanmunozmartins@gmail.com>
2023-08-30 19:43:31 -0300

    CardAvaliacao index.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,17 +11,36 @@ import Login from './pages/Login/index.tsx'
 import Privacidade from './pages/Privacidade/index.tsx'
 import Produtos from './pages/Produtos/index.tsx'
 
+//estilização global
+/* import "./index.css"; */
+
+//rotas
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CardAvaliacao from './components/CardAvaliacao/index.tsx'
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
-    <CadastroAdmFrota />
-    <CadastroEmpresa />
-    <CadastroGeral />
-    <CadastroOfertas />
-    <CadastroTipoUsuario />
-    <CadastroVeiculo />
-    <Login />
-    <Privacidade />
-    <Produtos />
-  </React.StrictMode>,
+  <BrowserRouter> {/*Indica que aplicação terá rotas*/}
+    {/* <Header /> */}
+    <Routes>
+    <Route path='/' element={<Home />} /> {/*Indica o caminho do componente e o nome da rota dele*/}
+
+      {/* <CadastroAdmFrota />
+      <CadastroEmpresa />
+      <CadastroGeral />
+      <CadastroOfertas />
+      <CadastroTipoUsuario />
+      <CadastroVeiculo />
+      <Login />
+      <Privacidade />
+      <Produtos /> */}
+    </Routes>
+   {/*  <Footer /> */}
+   </BrowserRouter>
+   </React.StrictMode>
+
 )
+
+
+
