c79f64062b1c08794edd7c693f92f998569ad582
Alan Munoz Martins <alanmunozmartins@gmail.com>
2023-08-30 20:14:16 -0300

    seta voltar link

--- src/components/SetaVoltar/index.tsx
+++ src/components/SetaVoltar/index.tsx
@@ -4,21 +4,17 @@ import { Link } from "react-router-dom";
 
 function seta_voltar() {
     return (
-        <div>
-            <div className="div_voltar">
-                <a href="../home/index.html#">
-                    <img
-                        className="seta_voltar"
-                        src="../cadastroTipoUsuario/img/img_setaVoltar-semFundo.png"
-                        alt=""
-                    />
-                </a>
-                <a href="../home/index.html#" className="aVoltar">
-                    Voltar
-                </a>
-            </div>
+        <>
+            <div className="div_voltar"><Link to={"../home/index.html#"}>voltar</Link>
 
-        </div>
+                <img
+                    className="seta_voltar"
+                    src="../cadastroTipoUsuario/img/img_setaVoltar-semFundo.png"
+                    alt=""
+                />
+
+            </div >
+        </>
     )
 }
 
