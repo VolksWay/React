d4083fbc9ff9b66d63d3120b552ed245cafd81b5
Fabricio Mitsuo Tanaka <24727827808@info.local>
2023-08-30 21:22:00 -0300

    botao VerMais

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './pages/Home/index.tsx'
-import CadastroAdmFrota from './pages/CadastroAdmFrota/index.tsx'
-import CadastroEmpresa from './pages/CadastroEmpresa/index.tsx'
-import CadastroGeral from './pages/CadastroGeral/index.tsx'
-import CadastroOfertas from './pages/CadastroOfertas/index.tsx'
-import CadastroTipoUsuario from './pages/CadastroTipoUsuario/index.tsx'
-import CadastroVeiculo from './pages/CadastroVeiculo/index.tsx'
-import Login from './pages/Login/index.tsx'
-import Privacidade from './pages/Privacidade/index.tsx'
-import Produtos from './pages/Produtos/index.tsx'
+// import Home from './pages/Home/index.tsx'
+// import CadastroAdmFrota from './pages/CadastroAdmFrota/index.tsx'
+// import CadastroEmpresa from './pages/CadastroEmpresa/index.tsx'
+// import CadastroGeral from './pages/CadastroGeral/index.tsx'
+// import CadastroOfertas from './pages/CadastroOfertas/index.tsx'
+// import CadastroTipoUsuario from './pages/CadastroTipoUsuario/index.tsx'
+// import CadastroVeiculo from './pages/CadastroVeiculo/index.tsx'
+// import Login from './pages/Login/index.tsx'
+// import Privacidade from './pages/Privacidade/index.tsx'
+// import Produtos from './pages/Produtos/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    {/* <Home />
     <CadastroAdmFrota />
     <CadastroEmpresa />
     <CadastroGeral />
@@ -22,7 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <CadastroVeiculo />
     <Login />
     <Privacidade />
-    <Produtos />
+    <Produtos /> */}
   </React.StrictMode>,
 )
 
