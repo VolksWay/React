c5153926444331d3e87666eaf06cebb2f68d2c1c
Fabricio Mitsuo Tanaka <24727827808@info.local>
2023-09-04 19:41:28 -0300

    BotaoCadastrar

--- src/components/BotaoCadastrar/index.tsx
+++ src/components/BotaoCadastrar/index.tsx
@@ -8,32 +8,13 @@ import "./style.css"
 import { Link } from "react-router-dom";
 
 
-function BotaoCadastrar (){
+function BotaoCadastrar() {
 
-    return(
-
-         
-        <>
-
-        <main id="BotaoCadastrar">
-
-        {/* <div className="home_botao_cadastrar">
-                  <Link  to="/" className=" home_btn_cadastrar"  >Cadastrar-se</Link>
-                  </div> */}
-
-        <div id="btn_cadastrar">
-
+    return (
         <Link to="/" className="btn_cadastrar">Cadastrar-se</Link>
-        
-        </div>
-        
-        </main>
+    )
 
-        </>
-
-)
-
-} 
+}
 export default BotaoCadastrar;
-    
+
 
