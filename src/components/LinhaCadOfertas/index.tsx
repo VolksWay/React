2538786261863e29d2e93e2cc740105f42da2f39
fiamaBrenda <fiamabrenda94@gmail.com>
2023-09-20 21:15:33 -0300

    Consumindo API CadastroOf\erta

--- src/components/LinhaCadOfertas/index.tsx
+++ src/components/LinhaCadOfertas/index.tsx
@@ -4,7 +4,7 @@ import Invisivel from "../../assets/img/invisivel.svg"
 
 import { Link/* , useLocation  */ } from "react-router-dom";
 
-function Linhas() {
+function Linhas(props: any) {
 
     return (
         <>
@@ -17,68 +17,12 @@ function Linhas() {
                     <img src={PencilDraw}alt="" />
                 </Link>
             </td>
-            <td>057</td>
-            <td>Frete gratis </td>
-            <td>Cupom</td>
-            <td>Banner Menu</td>
-            <td>Clientes volks</td>
-            <td>Todos os canais</td>
-            <td>
-                <img src={Visivel} alt="" />
-            </td>
-        </tr>
-                <tr className="cad_ofertas_campos">
-                <td>
-                    <input type="checkbox" />
-                </td>
-                <td>
-                    <Link to='#'>
-                        <img src={PencilDraw}alt="" />
-                    </Link>
-                </td>
-                <td>057</td>
-                <td>Frete gratis </td>
-                <td>Cupom</td>
-                <td>Banner Menu</td>
-                <td>Clientes volks</td>
-                <td>Todos os canais</td>
-                <td>
-                    <img src={Visivel} alt="" />
-                </td>
-            </tr>
-                    <tr className="cad_ofertas_campos">
-                    <td>
-                        <input type="checkbox" />
-                    </td>
-                    <td>
-                        <Link to='#'>
-                            <img src={PencilDraw}alt="" />
-                        </Link>
-                    </td>
-                    <td>057</td>
-                    <td>Frete gratis </td>
-                    <td>Cupom</td>
-                    <td>Banner Menu</td>
-                    <td>Clientes volks</td>
-                    <td>Todos os canais</td>
-                    <td>
-                        <img src={Visivel} alt="" />
-                    </td>
-                </tr>        <tr className="cad_ofertas_campos">
-            <td>
-                <input type="checkbox" />
-            </td>
-            <td>
-                <Link to='#'>
-                    <img src={PencilDraw}alt="" />
-                </Link>
-            </td>
-            <td>057</td>
-            <td>Frete gratis </td>
-            <td>Cupom</td>
-            <td>Banner Menu</td>
-            <td>Clientes volks</td>
-            <td>Todos os canais</td>
+            <td>{props.codigo}</td>
+            <td>{props.Nome_propaganda} </td>
+            <td>{props.Tipo_propaganda} </td>
+            <td>{props.Posicao_banner} </td>
+            <td>{props.usuario} </td>
+            <td>{props.canal} </td>
             <td>
                 <img src={Visivel} alt="" />
             </td>
